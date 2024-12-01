2d538249066a812596cea6e82d7b501dfbf0889c
Artur_Slabon <artur_slabon@epam.com>
2024-12-01 20:46:42 +0100

    Initial commit

---
+++ src/main/java/pl/slabonart/java_adv_bc_module_4/JavaAdvBcModule4Application.java
@@ -0,0 +1,13 @@
+package pl.slabonart.java_adv_bc_module_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvBcModule4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdvBcModule4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/slabonart/java_adv_bc_module_4/JavaAdvBcModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.slabonart.java_adv_bc_module_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdvBcModule4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
